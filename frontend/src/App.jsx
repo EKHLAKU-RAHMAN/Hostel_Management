11f4aaa4cca867cad3bd8c0e722eb2c7acf493f8
EKHLAKU-RAHMAN <mdrahman773958@gmail.com>
2026-01-22 23:26:14 +0530

    fix app file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 
 import React from "react";
-import { ToastContainer } from "react-toastify";
+// import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
