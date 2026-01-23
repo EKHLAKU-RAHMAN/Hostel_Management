25efc52b70b40f381b11c26fac61e823cdf45cb6
EKHLAKU-RAHMAN <mdrahman773958@gmail.com>
2026-01-23 11:59:38 +0530

    student login api fix

--- Backend/routes/studentLoginRoute.js
+++ Backend/routes/studentLoginRoute.js
@@ -1,44 +1,3 @@
-// const express = require("express");
-// const jwt = require("jsonwebtoken");
-// const Student = require("../models/Student");
-// const router = express.Router();
-//  const bcrypt = require("bcryptjs");
-
-// router.post("/student/login", async (req, res) => {
-//   console.log("Received body:", req.body); // 👈 add this line
-//   const { studentId, password } = req.body;
-
-//   if (!studentId || !password) {
-//     return res.status(400).json({ message: "Student ID and password are required" });
-//   }
-
-//   try {
-//     const student = await Student.findOne({ studentId });
-//     if (!student) {
-//       return res.status(404).json({ message: "No student found with this ID" });
-//     }
-
-//     // const isMatch = await student.comparePassword(password);
-//     const isMatch = await bcrypt.compare(password, student.password);
-//     console.log(isMatch);
-//     if (!isMatch) {
-//       return res.status(400).json({ message: "Invalid credentials" });
-//     }
-
-//     res.status(200).json({
-//       success: true,
-//       message: "Login successful",
-//       student,
-//     });
-//   } catch (err) {
-//     console.error("Login error:", err);
-//     res.status(500).json({ message: "Server error" });
-//   }
-// });
-
-
-// module.exports = router;
-
 
 const express = require("express");
 const jwt = require("jsonwebtoken");
@@ -59,12 +18,27 @@ router.post("/student/login", async (req, res) => {
       return res.status(404).json({ message: "No student found with this ID" });
     }
 
-    const bcrypt = require("bcryptjs");
-    const isMatch = await bcrypt.compare(password, student.password);
-    console.log(isMatch);
-    if (!isMatch) {
-      return res.status(400).json({ message: "Invalid credentials" });
-    }
+    
+    // const isMatch = await bcrypt.compare(password, student.password);
+    // console.log(isMatch);
+    // if (!isMatch) {
+    //   return res.status(400).json({ message: "Invalid credentials" });
+    // }
+
+    let isMatch = false;
+
+if (student.password.startsWith("$2")) {
+  // hashed password
+  isMatch = await bcrypt.compare(password, student.password);
+} else {
+  // old plain password
+  isMatch = password === student.password;
+}
+
+if (!isMatch) {
+  return res.status(401).json({ message: "Invalid credentials" });
+}
+
 
     // ✅ Generate JWT token
     const token = jwt.sign(
