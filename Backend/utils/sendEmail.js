c35ec4412fae5c695d1ecf6a24bd4de30ba9f0bf
EKHLAKU-RAHMAN <mdrahman773958@gmail.com>
2026-01-27 11:34:11 +0530

    fixes mail and npm

--- Backend/utils/sendEmail.js
+++ Backend/utils/sendEmail.js
@@ -1,41 +1,42 @@
-const nodemailer = require("nodemailer");
+// const nodemailer = require("nodemailer");
 
-const sendEmail = async ({ to, subject, html }) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS
-    },
-    tls: {
-    rejectUnauthorized: false,
-  },
-  connectionTimeout: 30000,
-  greetingTimeout: 30000,
-  socketTimeout: 30000,
-  });
+// const sendEmail = async ({ to, subject, html }) => {
+//   const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//       user: process.env.EMAIL_USER,
+//       pass: process.env.EMAIL_PASS
+//     },
+//     tls: {
+//     rejectUnauthorized: false,
+//   },
+//   connectionTimeout: 30000,
+//   greetingTimeout: 30000,
+//   socketTimeout: 30000,
+//   });
 
-  await transporter.sendMail({
-    from: `"Hostel Management" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    html
-  });
-};
+//   await transporter.sendMail({
+//     from: `"Hostel Management" <${process.env.EMAIL_USER}>`,
+//     to,
+//     subject,
+//     html
+//   });
+// };
 
-module.exports = sendEmail;
+// module.exports = sendEmail;
 
 
 // const { Resend } = require("resend");
+import { Resend } from "resend";
 
-// const resend = new Resend(process.env.RE-EMAIL_API_KEY);
+const resend = new Resend(process.env.RE-EMAIL_API_KEY);
 
-// module.exports = async function sendEmail({ to, subject, html }) {
-//   return resend.emails.send({
-//     from: `Hostel ERP <${process.env.EMAIL_USER}>`,
-//     to,
-//     subject,
-//     html,
-//   });
-// };
+module.exports = async function sendEmail({ to, subject, html }) {
+  return resend.emails.send({
+    from: `Hostel ERP <${process.env.EMAIL_USER}>`,
+    to,
+    subject,
+    html,
+  });
+};
 
